8ce1ac91f6cd6524402d4b888bf66c060f6e202e
r0984209 <seppe.dehouwer@student.kuleuven.be>
2026-03-03 00:09:00 +0100

    implementation of bid class

--- group05/src/Bid.java
+++ group05/src/Bid.java
@@ -1,2 +1,41 @@
-public class Bid {
+enum BidType {
+    PASS, ASK_PARTNER, SOLO, OPEN_MISERIE, MISERIE
+}
+
+/**
+ * @author Seppe De Houwer
+ * @since 24/2/26
+ */
+class Bid {
+    private Player bidder;
+    private BidType type;
+
+    /**
+     * @param bidder
+     * @param type
+     */
+    public Bid(Player bidder, BidType type) {
+        if (bidder == null) {
+            throw IllegalArgumentException();
+        }
+            this.bidder = bidder;
+        if (type == null) {
+            throw IllegalArgumentException();
+        }
+        this.type = type;
+    }
+
+    /**
+     * @return the bidder
+     */
+    public Player getBidder() {
+        return bidder;
+    }
+
+    /**
+     * @return the bidtype
+     */
+    public BidType getType() {
+        return type;
+    }
 }
