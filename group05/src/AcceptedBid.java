dc2a4faa6ae4ac67b1b0a3951eb3dc74761b76cd
Tommy Wu <tommy.wu@student.kuleuven.be>
2026-03-04 13:08:46 +0100

    feeat: AcceptedBid implemented

--- group05/src/AcceptedBid.java
+++ group05/src/AcceptedBid.java
@@ -1,14 +1,14 @@
 import java.util.List;
 
-public record SoloProposalBid(Player player) implements Bid {
+public record AcceptedBid(Player proposer, Player acceptor) implements Bid {
     @Override
     public List<Player> getTeam() {
-        return List.of(player);
+        return List.of(proposer, acceptor);
     }
 
     @Override
     public BidRank getRank() {
-        return BidRank.SOLO_PROPOSAL;
+        return BidRank.ACCEPTANCE;
     }
 
     @Override
@@ -19,14 +19,16 @@ public record SoloProposalBid(Player player) implements Bid {
     @Override
     public boolean checkWin(List<Trick> tricksWon) {
         if (tricksWon.isEmpty()) {return false;}
-        return tricksWon.size() >= 5 && tricksWon.getLast().hasTrump();
+        return tricksWon.size() >= 8 && tricksWon.getLast().hasTrump();
     }
 
     @Override
     public int calculateBasePoints(int tricksWon) {
-        int base = 6;
-        int extra = tricksWon - 5;
-        if (extra > 0) {base = base + 3*extra;}
+        int base = 2;
+        int extra = tricksWon - 8;
+        if (extra > 0) {
+            base = base + extra;
+        }
         return base;
     }
 }
