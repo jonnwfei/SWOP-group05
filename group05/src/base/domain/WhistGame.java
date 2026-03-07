f515e345817a3fefe17a67a27208c97e89dee6de
John Cai <john.cai@student.kuleuven.be>
2026-03-07 01:02:23 +0100

    chore: addition to Whist for getters and more

--- group05/src/base/domain/WhistGame.java
+++ group05/src/base/domain/WhistGame.java
@@ -6,10 +6,13 @@ import base.domain.round.Round;
 import base.domain.states.*;
 import cli.elements.GameEvent;
 
+/**
+ * @author Stan Kestens
+ * @since 28/02/2026
+ */
 public class WhistGame {
 
     private State state;
-    private boolean running;
     private List<Player> players;
     private List<Round> rounds;
     private Player currentPlayer;
@@ -17,25 +20,77 @@ public class WhistGame {
 
     public WhistGame(){
         this.state = new MenuState(this);
-        this.running = true;
         this.players = new ArrayList<>();
         this.rounds = new ArrayList<>();
         this.currentPlayer = null;
         this.dealerPlayer = null;
     }
+
+    /**
+     * Gets a shallow copy of the list of Players
+     *
+     * @return list of Player
+     */
     public List<Player> getPlayers(){
-        return this.players;
+        return new ArrayList<>(this.players);
     }
 
-    public void nextState(){
-        this.state = state.nextState();
+    /**
+     * Gets a shallow copy of the list of Rounds
+     *
+     * @return list of Rounds
+     */
+    public List<Round> getRounds(){
+        return new ArrayList<>(this.rounds);
     }
+
+    /**
+     * Gets the dealer of this Game
+     *
+     * @return dealerPlayer of this Game
+     */
+    public Player getDealerPlayer(){
+        return this.dealerPlayer;
+    }
+
+    /**
+     * Gets the active player of Game
+     *
+     * @return currentPlayer of Game
+     */
+    public Player getCurrentPlayer(){
+        return this.currentPlayer;
+    }
+
+
+    public State nextState(String finalAnswer){
+        return state.nextState();
+    }
+
     public GameEvent executeState(String response){
         return state.executeState(response);
     }
+
+    /**
+     * Adds given player to this Game, players
+     *
+     * @param player to add
+     */
     public void addPlayer(Player player){
         this.players.add(player);
     }
+
+    /**
+     * Returns a formatted string of the players in this game as follows:
+     * <pre>
+     * Players in this game:
+     * - player1
+     * - player2
+     * etc.
+     * </pre>
+     *
+     * @return the formatted player names
+     */
     public String printNames() {
         String result = "Players in this game:\n";
         for (Player p : players) {
