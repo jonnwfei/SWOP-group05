7b86f9e0af23cb1cca464f988fd2ca686042f213
stankestens <stankestens@yahoo.com>
2026-03-09 19:06:47 +0100

    part one of fixing IO, changes to Player.java is removing getFormattedHand(..) PlayState.java added bool to see if round is over, and deleted all the questionevents (TO BE CHECKED)

--- group05/src/cli/elements/ErrorEvent.java
+++ group05/src/cli/elements/ErrorEvent.java
@@ -1,6 +1,6 @@
 package cli.elements;
 
-public record ErrorEvent(int lowerBound, int upperBound) {
+public record ErrorEvent(int lowerBound, int upperBound) implements GameEvent{
     private void RenderErrorEvent(ErrorEvent event) {
         System.out.println("Please give a number between " + event.lowerBound + " and " + event.upperBound);
     }
