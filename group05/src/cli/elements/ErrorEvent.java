c1314cda7d1138aa8cabbc7ecf7e2b5c40a2261c
Towu04 <tommyyimingwu@gmail.com>
2026-03-09 23:38:35 +0100

    refactor events

--- group05/src/cli/elements/ErrorEvent.java
+++
@@ -1,7 +0,0 @@
-package cli.elements;
-
-public record ErrorEvent(int lowerBound, int upperBound) {
-    private void RenderErrorEvent(ErrorEvent event) {
-        System.out.println("Please give a number between " + event.lowerBound + " and " + event.upperBound);
-    }
-}
