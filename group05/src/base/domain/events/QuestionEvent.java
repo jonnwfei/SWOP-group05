0cd6c3047b49fd13dbb88d8df3a911a780f7f303
Towu04 <tommyyimingwu@gmail.com>
2026-03-09 23:38:35 +0100

    refactor events

--- group05/src/base/domain/events/QuestionEvent.java
+++ group05/src/base/domain/events/QuestionEvent.java
@@ -2,4 +2,8 @@ package base.domain.events;
 
 public record QuestionEvent(String string) implements GameEvent {
 
+    @Override
+    public boolean isInputRequired() {
+        return true;
+    }
 }
