a99dbb52604b329a1709b27e1d36323596be277c
Towu04 <tommyyimingwu@gmail.com>
2026-03-10 23:25:13 +0100

    allah

--- group05/src/base/domain/events/playevents/LastTrickEvent.java
+++ group05/src/base/domain/events/playevents/LastTrickEvent.java
@@ -5,14 +5,5 @@ import base.domain.trick.Trick;
 import base.domain.trick.Turn;
 
 public record LastTrickEvent(Trick trick) implements GameEvent {
-    private String handleLastTrickEvent(){
-        StringBuilder table = new StringBuilder("\n-------------- LAST PLAYED TRICK ---------------\n");
-
-        for (Turn turn : trick.getTurns()) {
-            table.append("- ").append(turn.toString()).append("\n");
-        }
-        table.append("------------------------------------------------\n");
-        return table.toString();
-    }
 }
 
