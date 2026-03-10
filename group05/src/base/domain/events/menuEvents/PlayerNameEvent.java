e12655e91187a08a847bbe8d615ee5fe505cf6d3
Towu04 <tommyyimingwu@gmail.com>
2026-03-10 02:01:50 +0100

    add menuEvents to Renderer

--- group05/src/base/domain/events/menuEvents/PlayerNameEvent.java
+++ group05/src/base/domain/events/menuEvents/PlayerNameEvent.java
@@ -2,9 +2,4 @@ package base.domain.events.menuEvents;
 
 import base.domain.events.GameEvent;
 
-public record PlayerNameEvent(int index) implements GameEvent {
-    private String renderPlayerNameEvent(){
-    return "Give the name of player" + index + ": ";
-    }
-    //geen voorwaarden op
-}
+public record PlayerNameEvent(int playerIndex) implements GameEvent {}
