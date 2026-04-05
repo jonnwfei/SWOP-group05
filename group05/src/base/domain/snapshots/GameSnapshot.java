3ebd0a5cca862397cca00d3e122b1cb7d185501f
John Cai <john.cai@student.kuleuven.be>
2026-04-06 01:43:57 +0200

    chore: added documentation, defensive checks and unit test

--- group05/src/base/domain/snapshots/GameSnapshot.java
+++ group05/src/base/domain/snapshots/GameSnapshot.java
@@ -3,6 +3,9 @@ package base.domain.snapshots;
 import java.util.List;
 
 /**
+ * @author John Cai
+ * @since 03/04/2026
+ *
  * Serializable snapshot of all data required to continue a session.
  * containing:
  * <ul>
@@ -11,7 +14,27 @@ import java.util.List;
  *      <li>Dealer index</li>
  *      <li>List of playerSnapshots</li>
  * </ul>
-
  */
 public record GameSnapshot(String description, SaveMode mode, Integer dealerIndex, List<PlayerSnapshot> players) {
+    /**
+     * Defensive constructor for GameSnapshot
+     * @param description description/alias/name for the save, used for choosing between saves when loading
+     * @param mode save mode, either full game save or count game save
+     * @param dealerIndex index of the dealer for the current round, used for determining turn order and game flow when loading
+     * @param players list of player snapshots, containing all player data
+     * @throws IllegalArgumentException if description is null
+     * @throws IllegalArgumentException if mode is null
+     * @throws IllegalArgumentException if dealer index is null
+     * @throws IllegalArgumentException if list of playerSnapshots is null
+     */
+    public GameSnapshot{
+        if (description == null) throw new IllegalArgumentException("Description of a playerSnapshot cannot be null");
+        if (mode == null) throw new IllegalArgumentException("Mode of a playerSnapshot cannot be null");
+        if (players == null) throw new IllegalArgumentException("Players of a playerSnapshot cannot be null");
+        if (dealerIndex == null) throw new IllegalArgumentException("Dealer index of a playerSnapshot cannot be null");
+        if (dealerIndex < 0) throw new IllegalArgumentException("Dealer index of a playerSnapshot cannot be negative");
+        if (dealerIndex > players.size() - 1) throw new IllegalArgumentException("Dealer index of a playerSnapshot cannot exceed player count");
+
+        players = List.copyOf(players);
+    }
 }
