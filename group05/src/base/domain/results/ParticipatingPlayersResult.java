45258a875a05722ade7364033864f512a7664ac7
stankestens <stankestens@yahoo.com>
2026-04-16 17:55:03 +0200

    final result

--- group05/src/base/domain/results/ParticipatingPlayersResult.java
+++ group05/src/base/domain/results/ParticipatingPlayersResult.java
@@ -2,6 +2,17 @@ package base.domain.results;
 
 import java.util.List;
 
-public record ParticipatingPlayersResult(List<String> playerNames, boolean multiSelect) implements GameResult {}
+public record ParticipatingPlayersResult(
+        List<String> playerNames,
+        boolean multiSelect
+) implements GameResult {
 
+    public ParticipatingPlayersResult {
+        if (playerNames == null || playerNames.isEmpty()) {
+            throw new IllegalArgumentException("playerNames cannot be null or empty");
+        }
+
+        playerNames = List.copyOf(playerNames);
+    }
+}
 
