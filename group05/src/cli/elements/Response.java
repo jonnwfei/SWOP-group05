9e0ee13ea0d31d74b6656b45699ecb3779317f22
John Cai <john.cai@student.kuleuven.be>
2026-04-17 19:17:11 +0200

    chore: added documentation, removed extra defensive check

--- group05/src/cli/elements/Response.java
+++ group05/src/cli/elements/Response.java
@@ -1,8 +1,11 @@
 package cli.elements;
 
+/**
+ * @author Stan Kestens
+ * @since 17/04/2026
+ *
+ * A simple record to encapsulate user input from the terminal.
+ * @param rawInput user input, can be null if the event did not require input.
+ */
 public record Response(String rawInput) {
-    public Response {
-        if (rawInput == null)
-            throw new IllegalArgumentException("Response must not contain null rawInput");
-    }
 }
