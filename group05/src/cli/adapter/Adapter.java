3f5e81cccb3bf086022e2acd6d32cd90c3b04f26
John Cai <john.cai@student.kuleuven.be>
2026-04-18 22:53:30 +0200

    chore: removed wrong line

--- group05/src/cli/adapter/Adapter.java
+++ group05/src/cli/adapter/Adapter.java
@@ -277,7 +277,6 @@ public class Adapter {
                 case ScoreBoardResult g -> {
                     int choice = parser.parseNumberInput(raw);
 
-                    if g.
                     if (choice < 1) {
                         throw new IllegalArgumentException("Invalid scoreboard selection");
                     }
