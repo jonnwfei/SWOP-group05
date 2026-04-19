6c69c68aeb342e124b84e3a3d93d4ba84295bb03
John Cai <john.cai@student.kuleuven.be>
2026-04-19 14:22:46 +0200

    chore: delete deprecated Action folder and shit

--- group05/src/base/domain/actions/NumberAction.java
+++
@@ -1,9 +0,0 @@
-package base.domain.actions;
-
-/**
- * Action carrying a single menu choice or index.
- * @param value The integer selected by the user.
- * @author Tommy
- * @since 10/03/2026
- * */
-public record NumberAction(int value) implements GameAction {}
