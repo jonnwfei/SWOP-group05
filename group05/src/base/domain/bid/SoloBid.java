868b85992fae322d315db05808046e072dbf9d73
Towu04 <tommyyimingwu@gmail.com>
2026-05-19 19:20:43 +0200

    extract scoring calculation and information from Bids

--- group05/src/base/domain/bid/SoloBid.java
+++ group05/src/base/domain/bid/SoloBid.java
@@ -35,17 +35,6 @@ public record SoloBid(BidType bidType, Suit trump) implements Bid {
     @Override
     public Suit determineTrump(Suit dealtTrump) {
         if (dealtTrump == null) {throw new IllegalArgumentException("Dealt trump suit cannot be null.");}
-        return trump;}
-
-
-    @Override
-    public int calculateBasePoints(int tricksWon) {
-        if (tricksWon < 0) {throw new IllegalArgumentException("there can't be negative tricks won.");}
-        int points = bidType.getBasePoints();
-        if (tricksWon < bidType.getTargetTricks()) {
-            points = -1 * points;
-            return points;
-        }
-            return points;
-        }
+        return trump;
+    }
 }
